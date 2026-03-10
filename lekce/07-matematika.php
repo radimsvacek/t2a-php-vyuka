c6409ce64e026534e3f16e1744b44de427c1a6f4
Radim Sváček <radim@webrs.cz>
2026-03-10 11:42:13 +0100

    Add third lesson - math

---
+++ lekce/07-matematika.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+==================================================
+    PROCVIČOVÁNÍ PHP – LEKCE 3 (pokročilejší)
+    Matematika
+==================================================
+
+Instrukce:
+· Nastavte si 3 strany do proměnných $a, $b, $c
+· Zobrazte nastavené hodnoty na stránce (a = 10 cm, …)
+· Určete, zda tento trojúhelník lze sestrojit, v případě, že nelze, vypíše se: trojúhelník nelze sestrojit a ukončí se vše
+· Určete, zda tento trojúhelník je rovnostranný, rovnoramenný, nebo obecný a vypište.
+· Vynechte dva řádky, provede se výpočet obvodu a obsahu (použijte Heronův vzorec 𝑆=√𝑠(𝑠−𝑎)(𝑠−𝑏)(𝑠−𝑐), kde 𝑠=𝑎+𝑏+𝑐2) podle zadání (odmocnina je sqrt($cislo)) a vypíše se: obvod (obsah) je hodnota
+*/
+
+
