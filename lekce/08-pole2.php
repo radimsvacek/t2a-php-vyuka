f00bf51e738be578156bd4ac344d9168fe6ca65a
Radim Sváček <radim@webrs.cz>
2026-03-17 12:08:19 +0100

    Add 8 lesson - array cycle

---
+++ lekce/08-pole2.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Nadefinujte si pole deseti celých čísel.
+ *
+ * 2. Doplňte nakonec tohoto pole další číslo, které se zadá z klávesnice.
+ *
+ * 3. Vypište údaj o počtu prvků v tomto poli.
+ *
+ * 4. Pomocí cyklu vypište všechny prvky tohoto pole od posledního k prvnímu.
+ *
+ * 5. Zjistěte, zda v tomto poli se vyskytuje hodnota 1, v případě, že ano, zjistěte, kolikrát se vyskytuje.
+ *
+ * 6. Určete maximum a vypište ho.
+ *
+ * 7. Každý sudý prvek tohoto pole zvětšete o 10.
+ *
+ * 8. Vypište toto pole pomocí cyklu foreach.
+ *
+ * 9. Vynechejte 2 řádky.
+ *
+ * 10. Vytvořte další pole, do kterého budete z klávesnice zadávat celá čísla, zadávání se ukončí v případě, že se zadá -1, tento prvek už nebude součástí pole.
+ *
+ * 11. Vypište, o kolik prvků má více, méně, nebo zda má stejný počet prvků toto pole oproti prvnímu poli.
+ *
+ * 12. Vypište střídavě prvky těchto polí: prvek_z_prvního prvek_z_druhého prvek_z_prvního prvek_z_druhého atd.
+ *
+ * 13. Vypište z prvního pole všechna sudá a z druhého pole všechna lichá čísla.
+ */
