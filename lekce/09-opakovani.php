38f064ef713c6282a92d12d015b689a293750a79
Radim Sváček <radim@webrs.cz>
2026-03-24 11:47:29 +0100

    Add lesson for loops and arrays revision

---
+++ lekce/09-opakovani.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * 1. Do dvou proměnných zadejte hodnotu začátku a konce intervalu (např. $a=10, $b=150)
+ * 2. Pomocí cyklu zjistěte počet lichých čísel v tomto intervalu a vypište tuto informaci.
+ * 3. Dále zjistěte, kolik čísel končí číslicí 1 nebo 7 a vypište tuto hodnotu.
+ * 4. Do pole uložte všechny hodnoty, pro které platí v daném intervalu všechny tyto podmínky:
+ * - trojciferné číslo
+ * - dělitelné 5
+ * - je menší než 500
+ * (může nastat případ, že pole bude prázdné)
+ * 5. Vypište pomocí foreach prvky tohoto pole, v případě, že pole je prázdné, vypište tuto informaci.
+ */
